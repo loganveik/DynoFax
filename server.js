cbd91aa01af4318df7b24734cd3648b4fff6d861
ljv29 <jumpswervelogan@gmail.com>
2020-07-27 16:22:51 -0700

    set up server and connection to dynofax api.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./routes/api-routes")(app);
+// require("./routes/htmlroutes")(app);
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT ' + PORT);
+});
